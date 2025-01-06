dffef6466693c75b21dbda0e7a3bd3800c695598
Andalib Iftakher <108351152+anda22101@users.noreply.github.com>
2025-01-06 21:54:29 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
 from flaskapp import app, db, socketio
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
+    if os.getenv('VERCEL'):  
+        db_path = '/tmp/site.db' 
+        if not os.path.exists(db_path):
+            with app.app_context():
+                db.create_all()  
+    else:
+       
+        with app.app_context():
+            db.create_all()
+
+    # Start the SocketIO server
     socketio.run(app, debug=True)
